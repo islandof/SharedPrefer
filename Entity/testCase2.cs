034a13ebb402f2c31ed58d3bc4d3817a15eaed27
islandof <156872@qq.com>
2015-01-17 10:28:31 +0800

    push to origin

--- Entity/testCase2.cs
+++ Entity/testCase2.cs
@@ -5,12 +5,19 @@ using System.Text;
 
 namespace Entity
 {
-    public class testCase2
-    {
-        public string ownercompanyname { get; set; }
-        public string chepaino { get; set; }
-        public string dthappen { get; set; }
-        public string zhalantype { get; set; }
-        public string condition { get; set; }
-    }
+	public class testCase2
+	{
+
+		public string alarmid { get; set; }
+
+		public string ownercompanyname { get; set; }
+
+		public string chepaino { get; set; }
+
+		public string dthappen { get; set; }
+
+		public string zhalantype { get; set; }
+
+		public string condition { get; set; }
+	}
 }
