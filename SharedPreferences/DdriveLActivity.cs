761d505797bbe033096c847e23b085f98c7e6668
islandof <156872@qq.com>
2015-01-17 10:19:07 +0800

    改善

---
+++ SharedPreferences/DdriveLActivity.cs
@@ -0,0 +1,69 @@
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+using Android.App;
+using Android.Content;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using Entity;
+using Newtonsoft.Json;
+
+namespace SharedPreferences
+{
+	[Activity (Label = "XamarinDemo", Icon = "@drawable/xs", ScreenOrientation = ScreenOrientation.Sensor)]
+	public class DdriveActivity : Activity
+	{
+		private WebClient mClient;
+		private Uri mUrl;
+		private List<testCase1> mTestcase1;
+		private ProgressBar mProgressBar;
+		//private Testcase1sAdapter mAdapter;
+		private ListView mListView;
+
+		protected override void OnCreate (Bundle bundle)
+		{
+			base.OnCreate (bundle);
+			// Set our view from the "main" layout resource
+			SetContentView (Resource.Layout.Ddrive);
+            
+			mListView = FindViewById<ListView> (Resource.Id.listView);
+			//mProgressBar = FindViewById<ProgressBar> (Resource.Id.progressBar1);
+			//mSearch = FindViewById<EditText>(Resource.Id.etSearch1);
+			//mContainer = FindViewById<LinearLayout>(Resource.Id.llContainer1);
+			//LoadXamarin();
+			mClient = new WebClient ();
+			mUrl = new Uri ("http://cloud.tescar.cn/vehicle/GetTboxalarmintimeData?isspec=1");
+			mClient.DownloadDataAsync (mUrl);
+			mClient.DownloadDataCompleted += mClient_DownloadDataCompleted;
+		}
+
+
+		void mClient_DownloadDataCompleted (object sender, DownloadDataCompletedEventArgs e)
+		{
+			RunOnUiThread (() => {
+				string json = Encoding.UTF8.GetString (e.Result);                
+				var fRows = JsonConvert.DeserializeObject<FormatRows> (json);
+				mTestcase1 = JsonConvert.DeserializeObject<List<testCase1>> (fRows.rows.ToString ());                
+				Testcase1sAdapter mAdapter1 = new Testcase1sAdapter (this, Resource.Layout.row_testcase1, mTestcase1);
+				FragmentTransaction transaction = FragmentManager.BeginTransaction ();
+				SlidingTabsFragment fragment = new SlidingTabsFragment (mTestcase1, mAdapter1);
+				transaction.Replace (Resource.Id.sample_content_fragment, fragment);
+				transaction.Commit ();
+				//mProgressBar.Visibility = ViewStates.Invisible;
+			});
+            
+		}
+
+		//public override bool OnCreateOptionsMenu(IMenu menu)
+		//{
+		//    MenuInflater.Inflate(Resource.Menu.actionbar_main, menu);
+		//    return base.OnCreateOptionsMenu(menu);
+		//}
+       
+	}
+}
+
